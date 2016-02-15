9dd747ac6e22b854f74d3cbc5bc5b549e13d3f3a
tcn <t.clasen@schuchert.de>
2016-02-15 11:24:53 +0100

    kleinere Anpassungen

--- rules/variables.js
+++ rules/variables.js
@@ -4,10 +4,10 @@ module.exports = {
   "no-delete-var": 1,
   "no-label-var": 2,
   "no-shadow-restricted-names": 2,
-  "no-shadow": 2,
+  "no-shadow": 0,
   "no-undef-init": 2,
-  "no-undef": 0,
+  "no-undef": 2,
   "no-undefined": 0,
   "no-unused-vars": 1,
-  "no-use-before-define": 1
+  "no-use-before-define": 0
 };
