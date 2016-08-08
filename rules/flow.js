d68070ca7a0761dbae917fbd7314e5af6f426c11
marudor <marudor@marudor.de>
2016-08-08 09:48:46 +0200

    disable require parameter

--- rules/flow.js
+++ rules/flow.js
@@ -1,6 +1,6 @@
 module.exports = {
   'flowtype/define-flow-type': 1,
-  'flowtype/require-parameter-type': 1,
+  'flowtype/require-parameter-type': 0,
   'flowtype/require-return-type': 0,
   'flowtype/type-id-match': 0,
   'flowtype/space-before-type-colon': [2, 'never'],
