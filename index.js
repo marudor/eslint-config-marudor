8ab730069c905bb650a1de7bcbe38559a4ddf762
marudor <marudor@marudor.de>
2019-08-31 14:58:32 +0200

    Update deps

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = {
   extends: [
     './base.js',
-    './typescript.js',
     './jest.js',
     './react.js',
     './sort-imports.js',
     './prettier.js',
+    './typescript.js',
   ],
 };
