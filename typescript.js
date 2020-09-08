5fe33aa57c7e21d1e1c803bffcb7664b805fda10
marudor <marudor@marudor.de>
2020-09-08 13:09:42 +0200

    update rules

--- typescript.js
+++ typescript.js
@@ -2,12 +2,35 @@ module.exports = {
   extends: ['plugin:import/typescript', 'prettier/@typescript-eslint'],
   overrides: [
     {
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
       rules: {
         'no-use-before-define': 0,
         '@typescript-eslint/array-type': 2,
-        '@typescript-eslint/consistent-type-definitions': [
-          'error',
-          'interface',
+        '@typescript-eslint/consistent-type-imports': 2,
+        '@typescript-eslint/no-unused-vars': [
+          2,
+          {
+            argsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+          },
+        ],
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/no-empty-interface': 0,
+        '@typescript-eslint/no-unsafe-member-access': 0,
+        '@typescript-eslint/no-unsafe-call': 0,
+        '@typescript-eslint/no-unsafe-assignment': 0,
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-unsafe-return': 0,
+        '@typescript-eslint/restrict-template-expressions': 0,
+        '@typescript-eslint/no-non-null-assertion': 0,
+        '@typescript-eslint/explicit-module-boundary-types': [
+          2,
+          {
+            allowArgumentsExplicitlyTypedAsAny: true,
+          },
         ],
       },
       plugins: ['@typescript-eslint'],
@@ -20,7 +43,6 @@ module.exports = {
       },
       parserOptions: {
         sourceType: 'module',
-        tsconfigRootDir: './',
       },
     },
   ],
