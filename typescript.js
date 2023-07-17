cea4d543e06c3f02bb1e33f57413b0fbcd044fc1
marudor <marudor@marudor.de>
2023-07-17 21:43:58 +0200

    chore: update deps

--- typescript.js
+++ typescript.js
@@ -4,7 +4,9 @@ module.exports = {
     {
       extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/stylistic',
+        'plugin:@typescript-eslint/stylistic-type-checked',
       ],
       rules: {
         'no-use-before-define': 0,
